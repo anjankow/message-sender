a9a1a2c60c2b1636af0dfcc19d807b2977010e26
anjankow <anjankow@protonmail.com>
2026-08-13 17:48:25 +0200

    implement queue

--- sender/internal/queue.go
+++ sender/internal/queue.go
@@ -1,12 +1,49 @@
 package internal
 
+import (
+	"bytes"
+	"context"
+	"fmt"
+)
+
+// Queue implements a thread safe FIFO queue.
 type Queue struct {
-	pool *MemPool
+	pool    *MemPool
+	buffers chan *bytes.Buffer
 }
 
-func NewQueue(messageSize int, capacity int) *Queue {
-	pool := NewMemPool(messageSize)
+func NewQueue(capacity int) (*Queue, error) {
+	if capacity <= 0 {
+		return nil, fmt.Errorf("invalid capacity: %d, must be > 0", capacity)
+	}
 	return &Queue{
-		pool: pool,
+		pool:    NewMemPool(),
+		buffers: make(chan *bytes.Buffer, capacity),
+	}, nil
+}
+
+// Enqueue adds a message to the end of the queue.
+// This operation is blocking if the queue capacity is exceeded.
+// In such case cancel the context to return early.
+func (q *Queue) Enqueue(ctx context.Context, message string) error {
+	buf := q.pool.GetBuffer()
+	buf.WriteString(message)
+	select {
+	case q.buffers <- buf:
+	case <-ctx.Done():
+		return fmt.Errorf("failed to enqueue: %w", ctx.Err())
+	}
+	return nil
+}
+
+// Dequeue removes and returns the first message from the queue.
+// This operation is blocking if the queue is empty.
+// In such case cancel the context to return early.
+func (q *Queue) Dequeue(ctx context.Context) (string, error) {
+	select {
+	case buf := <-q.buffers:
+		return buf.String(), nil
+	case <-ctx.Done():
+		return "", fmt.Errorf("failed to dequeue: %w", ctx.Err())
 	}
 }
---
+++ sender/internal/queue_test.go
@@ -0,0 +1,98 @@
+package internal_test
+
+import (
+	"context"
+	"sync"
+	"testing"
+	"time"
+
+	"github.com/anjankow/message-sender/sender/internal"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestQueue(t *testing.T) {
+	t.Run("InvalidCapacity", func(t *testing.T) {
+		_, err := internal.NewQueue(-1)
+		require.Error(t, err)
+		_, err = internal.NewQueue(0)
+		require.Error(t, err)
+	})
+	t.Run("FIFO", func(t *testing.T) {
+		ctx := t.Context()
+		q, err := internal.NewQueue(3)
+		require.NoError(t, err)
+
+		// Enqueue three messages
+		require.NoError(t, q.Enqueue(ctx, "a"))
+		require.NoError(t, q.Enqueue(ctx, "b"))
+		require.NoError(t, q.Enqueue(ctx, "c"))
+
+		// Assert that the messages are dequeued in the correct order
+		a, err := q.Dequeue(ctx)
+		require.NoError(t, err)
+		require.Equal(t, "a", a)
+		b, err := q.Dequeue(ctx)
+		require.NoError(t, err)
+		require.Equal(t, "b", b)
+		c, err := q.Dequeue(ctx)
+		require.NoError(t, err)
+		require.Equal(t, "c", c)
+	})
+	t.Run("MaxCapacityExceeded", func(t *testing.T) {
+		ctx, cancel := context.WithCancel(t.Context())
+		q, err := internal.NewQueue(2)
+		require.NoError(t, err)
+		require.NoError(t, q.Enqueue(ctx, "a"))
+		require.NoError(t, q.Enqueue(ctx, "b"))
+
+		enqueueDone := make(chan struct{}, 1)
+		var wg sync.WaitGroup
+		wg.Go(func() {
+			err := q.Enqueue(ctx, "c")
+			enqueueDone <- struct{}{}
+			assert.Error(t, err, context.Canceled)
+		})
+
+		// Assert that Enqueue blocks when max capacity is exceeded
+		// and continue after 100 ms to
+		select {
+		case <-enqueueDone:
+			wg.Wait()
+			t.Errorf("expected to block on Enqueue when max capacity exceeded")
+		case <-time.After(100 * time.Millisecond): // sufficient time for Enqueue to be called
+		}
+
+		// Now cancel the context
+		cancel()
+		wg.Wait()
+	})
+	t.Run("Multithreaded", func(t *testing.T) {
+		ctx := t.Context()
+		capacity := 200
+		q, err := internal.NewQueue(capacity)
+		require.NoError(t, err)
+
+		start := make(chan struct{})
+
+		var wg sync.WaitGroup
+		for range 200 {
+			wg.Go(func() {
+				<-start
+				err := q.Enqueue(ctx, "a")
+				assert.NoError(t, err)
+			})
+
+			wg.Go(func() {
+				<-start
+				d, err := q.Dequeue(ctx)
+				assert.NoError(t, err)
+				assert.Equal(t, "a", d)
+			})
+		}
+
+		// Start at the same moment
+		close(start)
+		wg.Wait()
+	})
+}
